28d98012f51ca462da631654cbd3db9db45461d1
Pttr <draivv@yandex.ru>
2024-05-01 16:32:23 -0400

    Initial Commit

---
+++ CarefulScroll/CarefulScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulScrollApp.swift
+//  CarefulScroll
+//
+//  Created by P on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarefulScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
